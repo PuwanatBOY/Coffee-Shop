d69bdd8614590ad31759c1d539d02070c36587fa
ปิยะพงษ์ กิตติชัยวัฒนา <emma2541@outlook.com>
2020-06-03 16:17:45 +0700

    close#15 get product to product list

--- client/src/app/component/product-list/product-list.component.ts
+++ client/src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { AddListService } from 'src/app/service/add-list.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  sts:number = 1;
+  alldata: any;
+  term: string;
+
+  constructor(private router: Router,private addListService: AddListService) { }
 
   ngOnInit(): void {
   }
 
+  getAllData(){
+    if(this.sts == 1){
+    this.addListService.getAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.alldata = response;
+          console.log(this.alldata);
+        },
+        error => {
+          console.log(error);
+        });
+    }
+    this.sts = 0;
+    return this.alldata;
+  }
+
+  Logout(){
+    this.router.navigate(['/home']);
+  }
+  
 }
