30216a8bf1d86107eb4a874369b5f7c054f2ad20
PuwanatBOY <puwanattorcheewee@gmail.com>
2020-06-22 19:07:00 +0700

    new update

--- client/src/app/service/order-history.service.ts
+++ client/src/app/service/order-history.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl = 'http://209.97.163.81:3000/api/orderHistory';
+const baseUrl = 'http://localhost:3000/api/orderHistory';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderHistoryService {
 
-  constructor(private http: HttpClient) { }
+  t_id: number
+  t_name: string
+
+  status_type: any = [
+    {t_id: 1,t_name:"เสร็จสิ้น"},
+    {t_id: 2,t_name:"ยกเลิกการสั่งซื้อ"}
+  ]
+
+  constructor(private http: HttpClient) {}
+
+  getAllStatusType(){
+    return this.status_type;
+  }
+
   create(data) {
     return this.http.post(baseUrl, data);
   }
@@ -19,5 +32,9 @@ export class OrderHistoryService {
 
   findOrder(id) {
     return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  findOrderAll() {
+    return this.http.get(`${baseUrl}/`);
   }
 }
